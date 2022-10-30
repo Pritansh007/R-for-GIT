ba1508afe658d2774f84d07bfb1213948f828b39
Pritansh Tayade <pritanshtayade01@gmail.com>
2022-10-30 01:30:47 -0500

    Size=5

--- Rcode for Github.R
+++ Rcode for Github.R
@@ -3,4 +3,4 @@ require(ggplot2)
 require(dplyr)
 
 ## Scatter Plot
-ggplot2::ggplot(data = iris, aes(x = Sepal.Length, y=Sepal.Width, color = Species))+geom_point()
+ggplot2::ggplot(data = iris, aes(x = Sepal.Length, y=Sepal.Width, color = Species))+geom_point(size = 5)
